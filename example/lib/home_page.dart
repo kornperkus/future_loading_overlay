60269d176da9d3529d22095a52c756cb7fad6cb1
Kornperkus <percurz@gmail.com>
2022-12-24 19:15:16 +0700

    add example usage of future_loading_overlay

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:future_loading_overlay/future_loading_overlay.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +11,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int? _number;
+
+  Future<int> _getNumberFuture() async {
+    // Deley the future by 3 seconds
+    await Future.delayed(const Duration(seconds: 3));
+
+    return Future.value(Random().nextInt(10));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +29,26 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [],
+          children: [
+            const Text('Press a button below to run a future'),
+            if (_number != null) Text('Future completed with number $_number'),
+            ElevatedButton(
+              onPressed: () async {
+                final result = await showFutureLoadingOverlay<int>(
+                  context: context,
+                  future: _getNumberFuture(),
+                  expanded: false,
+                );
+
+                if (mounted) {
+                  setState(() {
+                    _number = result;
+                  });
+                }
+              },
+              child: const Text('Start'),
+            ),
+          ],
         ),
       ),
     );
